f433432d884f8c96dc6fb396c251d5a9a8f7caa5
trainandhire <projects.trainandhire@gmail.com>
2023-08-22 18:04:38 +0530

    features and permissions table implemented

--- src/app/features/institute-on-bording/features-permissions/features-permissions.component.ts
+++ src/app/features/institute-on-bording/features-permissions/features-permissions.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { BlockUI, NgBlockUI } from 'ng-block-ui';
 import { PermissionsService } from 'src/app/_api/roles and permissions/permissions.service';
 
@@ -7,6 +8,7 @@ import { PermissionsService } from 'src/app/_api/roles and permissions/permissio
   templateUrl: './features-permissions.component.html',
   styleUrls: ['./features-permissions.component.css']
 })
+
 export class FeaturesPermissionsComponent {
 
   public imageOptions = {
@@ -16,20 +18,79 @@ export class FeaturesPermissionsComponent {
     reload: true
   };
 
-  public featuresAndPermissions:any = [];
+  public featuresAndPermissions: any = [];
+
+  public multipleMultiSelect: any = [];
+  public multipleChapterMultiSelect: any = [];
+
+  public multipleSelectArray: any;
+  public multipleChapterSelectArray: any;
 
   @BlockUI('imageGallery') blockUIImageGallery: NgBlockUI;
 
-  constructor(private _permissionsService:PermissionsService){
+  constructor(private _permissionsService: PermissionsService, private modalService: NgbModal) {
+    this.multipleMultiSelect = [];
+    this.multipleChapterMultiSelect = [];
     this.getAllFeaturesAndPermissions();
+    this.multipleSelectArray = [
+      {
+        "item_id": 1, "item_text": "S21"
+      },
+      {
+        "item_id": 2, "item_text": "S22"
+      },
+      {
+        "item_id": 3, "item_text": "S23"
+      },
+      {
+        "item_id": 4, "item_text": "S24"
+      },
+      {
+        "item_id": 5, "item_text": "S25"
+      },
+      {
+        "item_id": 6, "item_text": "S26"
+      },
+      {
+        "item_id": 7, "item_text": "S27"
+      }
+    ]
+    this.multipleChapterSelectArray = [
+      {
+        "item_id": 1, "item_text": "ASDF JKL"
+      },
+      {
+        "item_id": 2, "item_text": "ASDFG JKL"
+      },
+      {
+        "item_id": 3, "item_text": " RTYU VBN"
+      },
+      {
+        "item_id": 4, "item_text": "QWE IOP"
+      },
+      {
+        "item_id": 5, "item_text": " ZXC NM"
+      },
+      {
+        "item_id": 6, "item_text": "ALL LETTERS"
+      }
+    ]
   }
 
-  getAllFeaturesAndPermissions(){
+  getAllFeaturesAndPermissions() {
     this._permissionsService.getAllFeaturesAndPermissions().subscribe(
-      (data:any)=>{
+      (data: any) => {
         this.featuresAndPermissions = data;
       }
     )
+  }
+
+  AllMailBodies(AllMailBodiesContent) {
+    this.modalService.open(AllMailBodiesContent, { windowClass: 'animated fadeInDown', size: 'lg' });
+  }
+
+  EditFeature(EditFeatureContent) {
+    this.modalService.open(EditFeatureContent, { windowClass: 'animated fadeInDown', size: 'lg' });
   }
 
   reloadImageGallery() {
@@ -39,7 +100,4 @@ export class FeaturesPermissionsComponent {
       this.blockUIImageGallery.stop();
     }, 2500);
   }
-
-
-
 }
